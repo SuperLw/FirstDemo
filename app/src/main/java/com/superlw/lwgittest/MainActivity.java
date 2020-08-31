fcb9370ec8d5c487f912b5f67452578fc6fc32ef
5CG75149CL\liuwei1 <1115856293@qq.com>
2020-08-31 09:22:24 +0800

    第一次提交

---
+++ app/src/main/java/com/superlw/lwgittest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.superlw.lwgittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
